14167f81b843c6006cfc334906bda3f71063c48f
thecodefather <owuorr11@hotmail.com>
2015-06-05 23:07:38 -0400

    Browse layout checkin all

---
+++ app/src/main/java/com/thecodefather/Helpers/Constants.java
@@ -0,0 +1,23 @@
+package com.thecodefather.Helpers;
+
+/**
+ * Created by roberto on 5/24/15.
+ */
+public class Constants {
+
+    public static String BAR = "Bar";
+    public static String NIGHTCLUB = "Night Club";
+    public static String CASINO = "Casino";
+    public static String MEAL_TAKEAWAY = "Meal Takeaway";
+    public static String RESTAURANT = "Restaurant";
+    public static String FOOD = "Food";
+    public static String CAFE = "Cafe";
+    public static String COFFEE = "Coffee";
+    public static String BAKERY = "Bakery";
+    public static String DINER = "Diner";
+    public static String MEAL_DELIVERY = "Meal Delivery";
+    public static String LIQUOR_STORE = "Liquor Store";
+    public static String MOVIE_THEATER = "Movie Theater";
+    public static String SHOPPING_MALL = "Shopping Mall";
+    public static String ESTABLISHMENT = "Establishment";
+}
